9c0aa94b5a3d61d34a2bcfa4d0bcf6d406b1ad0d
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2020-10-06 21:50:58 +0200

    Initial Create...

---
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using StockAnalyzer.Core.Domain;
+
+namespace StockAnalyzer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            #region Before loading stock data
+            var watch = new Stopwatch();
+            watch.Start();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+            #endregion
+
+            //var client = new HttpClient();
+            using(var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(result);
+
+                    Stocks.ItemsSource = data;
+                }catch(Exception ex)
+                {
+                    this.Notes.Text += $" Error happened {ex.Message} \n.";
+                }
+                
+            }
+
+            
+
+            #region After stock data is loaded
+            StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+            #endregion
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
